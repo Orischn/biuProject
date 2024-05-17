845ccc011e32cd1e97ea92fef53f631d3390ce6c
NadavRam1 <nadavr2005@gmail.com>
2024-05-17 17:40:24 +0300

    fixed typo

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,7 @@ const receiveAllUsers = async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     const users = await getUsers();
-    if (user === 401) {
+    if (users === 401) {
         return res.status(401).end();
     } else {
         return res.status(200).end(JSON.stringify(users));
