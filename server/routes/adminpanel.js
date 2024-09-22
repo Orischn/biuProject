f35391546ed4e53a26d142fa82cd943ce18943a2
Orischn <orischn02@gmail.com>
2024-09-22 16:59:16 +0300

    added an update grade function

--- server/routes/adminpanel.js
+++ server/routes/adminpanel.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { uploadTree, checkAdmin } = require('../controllers/adminpanel.js');
 const { validateUser } = require("../controllers/token.js");
-const { removeUser, createUser, receiveUser, receiveAllUsers, changePermissions } = require('../controllers/user.js')
+const { removeUser, createUser, receiveUser, receiveAllUsers, changePermissions } = require('../controllers/user.js');
+const { changeGrade } = require('../controllers/chatbot.js');
 const router = express.Router();
 
 router.post('/api/uploadDecisionTree/', validateUser, checkAdmin, uploadTree);
@@ -9,5 +10,6 @@ router.get('/api/users/:username', validateUser, checkAdmin, receiveUser);
 router.get('/api/users/', validateUser, checkAdmin, receiveAllUsers);
 router.post('/api/createUser/',validateUser, checkAdmin, createUser);
 router.post('/api/deleteUser/', validateUser, checkAdmin, removeUser);
+router.post('/api/updateGrade', validateUser, checkAdmin, changeGrade)
 router.post('/api/changePermissions/', validateUser, checkAdmin, changePermissions)
 module.exports = router;
